87b4bcd3703a38b49c84d699e887e836e9882b00
Chasse Rush <crush4799@gmail.com>
2020-06-04 13:47:46 -0400

    Updates

---
+++ precache-manifest.f45bafd02721b1fbef34602837c8c3f5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba4287c004cc9fab7f20c872f43f81a",
+    "url": "/StarWarsGraphqlSearch/index.html"
+  },
+  {
+    "revision": "092976053ffea62b36d2",
+    "url": "/StarWarsGraphqlSearch/static/css/main.f1509909.chunk.css"
+  },
+  {
+    "revision": "1bde40d60184b258b3e7",
+    "url": "/StarWarsGraphqlSearch/static/js/2.52a99053.chunk.js"
+  },
+  {
+    "revision": "28164e9e1f7b49ce7422bf240655ba5b",
+    "url": "/StarWarsGraphqlSearch/static/js/2.52a99053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092976053ffea62b36d2",
+    "url": "/StarWarsGraphqlSearch/static/js/main.5e773605.chunk.js"
+  },
+  {
+    "revision": "4f691e7f5f5b7c0b1c93",
+    "url": "/StarWarsGraphqlSearch/static/js/runtime-main.c1042436.js"
+  },
+  {
+    "revision": "3be6e68f03cb3585858cb1eb008aa2c8",
+    "url": "/StarWarsGraphqlSearch/static/media/ANH.3be6e68f.jpg"
+  },
+  {
+    "revision": "d9d3d588739e18e966f3d9b2f028ed33",
+    "url": "/StarWarsGraphqlSearch/static/media/Obi-wan.d9d3d588.jpg"
+  },
+  {
+    "revision": "cf88e2079455f3043d83e5110b983da3",
+    "url": "/StarWarsGraphqlSearch/static/media/Star_Wars_Death Star.cf88e207.svg"
+  },
+  {
+    "revision": "513ef4a01cd54927c72268bea168967e",
+    "url": "/StarWarsGraphqlSearch/static/media/Star_Wars_Falcon.513ef4a0.svg"
+  },
+  {
+    "revision": "07bee40491ecec919213156e58e55fa2",
+    "url": "/StarWarsGraphqlSearch/static/media/chewy.07bee404.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/StarWarsGraphqlSearch/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f2881ec0cc2d5c56a0b1fc416ce1fc05",
+    "url": "/StarWarsGraphqlSearch/static/media/x-wing2.f2881ec0.jpeg"
+  }
+]);
